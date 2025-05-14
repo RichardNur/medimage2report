3eea73d7b80aca5e61e2f5f27b83de9a344f8449
deburic <richard_nuernberger@outlook.com>
2025-05-14 13:23:58 +0200

    Completed successful user-registration

--- run.py
+++ run.py
@@ -28,6 +28,10 @@ data_manager = DataManagerInterface(db_file_name, app)
 login_manager = LoginManager()
 login_manager.init_app(app)
 
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(user_id)
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
